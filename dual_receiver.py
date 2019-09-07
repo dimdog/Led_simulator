8b555d6477047424c6338b2bc150d4fb7ada2054
Benjamin Reiter <ben@wake.io>
2019-09-07 09:24:14 -0700

    backed up work done at burning man

---
+++ dual_receiver.py
@@ -0,0 +1,116 @@
+import pygame
+import time
+import random
+import serial
+import struct
+
+import strands
+import redis
+from patterns import RandomPattern, SimpleRandomPattern
+
+ser = serial.Serial('/dev/tty.usbmodem14201', baudrate=1000000)
+ser2 = serial.Serial('/dev/tty.usbmodem14101', baudrate=1000000)
+
+rand = random.Random()
+pygame.init()
+height = 600
+width = 800
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("LED SIMULATOR")
+
+# make n strands of x length inputtable
+
+s0 = strands.Strand(320)
+rp0 = SimpleRandomPattern(s0)
+#rp0 = RandomPattern(s0)
+sm = strands.PatternManager(screen, height, width)
+sm.patterns.append(rp0)
+#sm.patterns.append(rp1)
+#sm.patterns.append(rp2)
+#sm.patterns.append(rp3)
+#sm.patterns.append(rp4)
+
+done = False
+r = redis.StrictRedis(host="localhost", port=6379, password="", decode_responses=True)
+
+
+# Protocol for wire.
+# 1. A number, to specify which strip is being accessed - e.g. `0`
+# 2. A number, specifying which type of data we are sending
+#   I. 0 means individual leds
+#   II. 1 means color wipe
+# 3. Data for the protocal type
+#   Individual LEDS: R G B (Repeated)
+#   Color Wipe R G B
+def individual_leds(serial):
+    for i, pattern in enumerate(sm.patterns):
+        # Protocol # 1
+        serial.write(struct.pack('>B',i))
+        # Protocol # 2
+        serial.write(struct.pack('>B', 0))
+        for color in pattern.strand.colors:
+            if serial:
+                serial.write(struct.pack('>B', min(254, color[0])))
+                serial.write(struct.pack('>B', min(254,color[1])))
+                serial.write(struct.pack('>B', min(254,color[2])))
+                #printed = False
+                #while serial.in_waiting or not printed:
+                #    printed = True
+                #    print(serial.readline())
+        serial.write(struct.pack('>B', 255))
+
+def wipe_all_strips():
+    for i, pattern in enumerate(sm.patterns):
+        for color in pattern.strand.colors[:1]:
+            color_wipe(ser, i, color)
+            color_wipe(ser2, i, color)
+
+
+def color_wipe(serial, strip_number, color):
+    # Protocol # 1
+    serial.write(struct.pack('>B', strip_number))
+    # Protocol # 2
+    serial.write(struct.pack('>B', 1))
+    # Protocol # 3
+    #print("Red:{}, Green:{}, Blue:{}".format(color[0], color[1], color[2]))
+    serial.write(struct.pack('>B', color[0]))
+    serial.write(struct.pack('>B', color[1]))
+    serial.write(struct.pack('>B', color[2]))
+    #printed = False
+    #while serial.in_waiting or not printed:
+    #    printed = True
+    #    print(serial.readline())
+
+
+def init_serial(serial):
+    while not serial.in_waiting: # warm up the serial connection
+        data = serial.write(struct.pack('>B', 255))
+        time.sleep(0.01)
+    while serial.in_waiting: # clear the buffer
+        serial.readline()
+
+init_serial(ser)
+init_serial(ser2)
+while not done:
+        for event in pygame.event.get():
+            if event.type == 2: # keydown
+                print(event)
+                print(event.unicode)
+                try: 
+                    a = int(event.unicode)
+                    rp0.change_color_range(a)
+                except:
+                    pass
+            if event.type == pygame.QUIT:
+                done = True
+        msg = r.rpop("beat_queue") 
+        data = msg 
+        for pattern in sm.patterns:
+            pattern.msg(data)
+        sm.display()
+        individual_leds(ser)
+        individual_leds(ser2)
+        #wipe_all_strips()
+        pygame.display.flip()
+
+
--- patterns.py
+++ patterns.py
@@ -1,35 +1,88 @@
 import time
 import random
+import colour
 
-
+# Red --> OrangeRed (Default)
+# Red --> Tomato (warming up)
+# DarkOrange --> OrangeRed (good)
+# DarkCyan --> Red (PARTY)
+# Red --> Blue (PARTY!!)
+# Green --> Blue (Good for blue mode) 
 class Pattern(object):
 
     def __init__(self, strand):
         self.strand = strand
         self.rand = random.Random()
+        self.counter = 0
+        self.color = None
+        color1 = colour.Color("Green")
+        color2 = colour.Color("Blue")
+        self.set_color_range(color1, color2)
+
+
+    def set_color_range(self, color1, color2):
+        hsl_cs = colour.color_scale(color1.get_hsl(), color2.get_hsl(), 100)
+        rgb_cs = [colour.hsl2rgb(a) for a in hsl_cs]
+        self.scaled_cs = []
+        for a in rgb_cs:
+            self.scaled_cs.append((round(a[0] * 255), round(a[1] * 255), round(a[2] * 255)))
 
     def msg(self, msg):
         # take a message and return the strands colors
         return self.strand.colors
+
+    def change_color_range(self, mode):
+        # (1) Red --> OrangeRed (Default)
+        # (2) Red --> Tomato (warming up)
+        # (3) DarkOrange --> OrangeRed (good)
+        # (4) DarkCyan --> Red (PARTY)
+        # (5) Red --> Blue (PARTY!!)
+        # (6) Green --> Blue (Good for blue mode) 
+        if mode == 1:
+            self.set_color_range(colour.Color("Red"), colour.Color("OrangeRed"))
+        elif mode == 2:
+            self.set_color_range(colour.Color("Red"), colour.Color("Tomato"))
+        elif mode == 3:
+            self.set_color_range(colour.Color("DarkOrange"), colour.Color("OrangeRed"))
+        elif mode == 4:
+            self.set_color_range(colour.Color("DarkCyan"), colour.Color("Red"))
+        elif mode == 5:
+            self.set_color_range(colour.Color("Red"), colour.Color("Blue"))
+        elif mode == 6:
+            self.set_color_range(colour.Color("Green"), colour.Color("Blue"))
 
 
 class RandomPattern(Pattern):
 
     def msg(self, msg):
         red = self.rand.randint(0,255)
-        green = self.rand.randint(0,255)
-        blue = self.rand.randint(0,255)
-        self.strand.add_rgb(red, green, blue)
+        self.strand.add_rgb(red,0, 0)
         return self.strand.colors
 
 class SimpleRandomPattern(Pattern):
+    """Silver, Firebrick"""
 
     def msg(self, msg):
+        if not self.color:
+            col = self.rand.randint(0, 100)
+            self.color = self.scaled_cs[col]
+
         if msg == "Beat":
-            red = self.rand.randint(0,255)
-            green = self.rand.randint(0,255)
-            blue = self.rand.randint(0,255)
-            self.strand.add_rgb(red, green, blue)
+            col = self.rand.randint(0, 100)
+            self.color = self.scaled_cs[col]
+            for i in range(50):
+                self.strand.add_rgb(self.color[0], self.color[1], self.color[2])
+        else:
+            add = 1
+            try: 
+                msg = int(msg)
+                add += max(0, msg)
+            except:
+                pass
+            col = self.rand.randint(0, 100)
+            self.color = self.scaled_cs[col]
+            for i in range(add):
+                self.strand.add_rgb(self.color[0], self.color[1], self.color[2])
         return self.strand.colors
 
 
